efa1be75648b54c7835abacd83487d15b25e03e6
Saniya Thahseen M <ayinasmadala@gmail.com>
2019-10-23 16:25:53 +0530

    saniya1

---
+++ registration.js
@@ -0,0 +1,5 @@
+var name=document.getElementById("name");
+var password=document.getElementById("password");
+var myObj = { name: name, password: password};
+var myJSON = JSON.stringify(myObj);
+localStorage.setItem("testJSON", myJSON);//storing data
