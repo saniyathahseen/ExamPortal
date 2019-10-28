0273805161c32238b86d01f69d0e945479ec749c
Saniya Thahseen M <ayinasmadala@gmail.com>
2019-10-28 17:12:40 +0530

    Update reg_web.php

--- reg_web.php
+++ reg_web.php
@@ -4,34 +4,33 @@
           Exam Portal
     </title>
     <style>
-    
     * {
         box-sizing: border-box;
-      }
+    }
 
-      /* Create two equal columns that floats next to each other */
+    /* Create two equal columns that floats next to each other */
     .column1 {
         float: left;
         width: 50%;
         padding: 10px;
         height: 600px; 
         background-image: url('exam.png');
-      }
+    }
     .column2 {
         float: left;
         width: 50%;
         padding: 10px;
         height: 300px;
-      }
-      /* Clear floats after the columns */
+    }
+    /* Clear floats after the columns */
     .row:after {
         content: "";
         display: table;
         clear: both;
-      }
+    }
     * {
         box-sizing: border-box;
-      }
+    }
 
     input, select, textarea {
         width: 100%;
@@ -39,26 +38,26 @@
         border: 1px solid #ccc;
         border-radius: 4px;
         resize: vertical;
-      }
+    }
 
 
     label {
         padding: 12px 12px 12px 0;
         display: inline-block;
-      }
+    }
 
     input[type=submit] {
-        background-color: #4CAF50;
+        background-color:#AED6F1;
         color: white;
         padding: 12px 20px;
         border: none;
         border-radius: 4px;
         cursor: pointer;
         float: right;
-      }
+    }
 
     input[type=submit]:hover {
-        background-color:#AED6F1 ;
+        background-color:lightgrey ;
     }
 
     .container {
@@ -71,31 +70,33 @@
         float: left;
         width: 25%;
         margin-top: 6px;
-      }
+    }
 
     .col-75 {
         float: left;
         width: 75%;
         margin-top: 6px;
-      }
+    }
 
-      /* Clear floats after the columns */
-      .row:after {
+    /* Clear floats after the columns */
+    .row:after {
         content: "";
         display: table;
         clear: both;
-      }
+    }
 
-      /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
-      @media screen and (max-width: 600px) {
+    /* Responsive layout - when the screen is less than 600px wide, make the two columns stack on top of each other instead of next to each other */
+    @media screen and (max-width: 600px) {
         .col-25, .col-75, input[type=submit] {
             width: 100%;
             margin-top: 0;
-          }
-      }
+        }
+    }
     h2{
-
-      }
+        font-family: 'Courier New', Courier, monospace;
+        color: #5DADE2 ;
+        font-size: 60px;
+    }
     </style>
   </head>
   <body>
